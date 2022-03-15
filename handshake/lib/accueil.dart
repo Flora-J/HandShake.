5e2cffcf34f6e112011ff2f564e6af9b59de50bf
Flora JULIE <flora.juli88@gmail.com>
2022-03-15 15:41:58 +0100

    Finalisation de la page d'accueil

--- handshake/lib/accueil.dart
+++ handshake/lib/accueil.dart
@@ -27,36 +27,33 @@ class Page1InteractionState extends State<Page1Interactive> {
     var size = MediaQuery.of(context).size;
     //Page2Interactive page2 = Page2Interactive();
 
+
     return Scaffold(
       key: const Key("Scaffoll"),
       appBar: AppBar(
         elevation: 30,
         title: const Text(
-          "HandShake",
+          "Accueil",
         ),
         centerTitle: true,
       ),
       body: Container(
-        padding: const EdgeInsets.only(top: 200),
+        padding: const EdgeInsets.only(top: 80),
         width: size.width,
         height: size.height,
         decoration: fondDecoration(),
         child: Column(children: [
           Container(
-            key: const Key("TextContainer"),
-            child: TitleDecoration("HandShake"),
-          ),
-          Container(
             key: const Key("iconContainer"),
-            margin: const EdgeInsets.only(top: 40),
-            child: const Icon(
-              Icons.handshake_outlined,
-              color: Colors.white,
-              size: 67,
-            ),
+            margin: const EdgeInsets.only(top:0),
+            child: logo(size),
+          ),
+          Container (
+            key: const Key("TextContainer"),
+            child: TitleDecoration("La fleur bleue de l'épanouissement \n des autistes"),
           ),
           Container(
-            margin: EdgeInsets.only(top: 150),
+            margin: EdgeInsets.only(top: 120),
             key: const Key("selectionContainer"),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -86,14 +83,25 @@ class Page1InteractionState extends State<Page1Interactive> {
     ));
   }
 
+  Image logo(Size size) {
+    return Image.asset (
+      "images/logo-handshake.png",
+      fit: BoxFit.contain,
+      height: size.shortestSide,
+      width: size.shortestSide,
+      alignment: Alignment.center,
+    );
+  }
+
   Text TitleDecoration(String text) {
     return Text(
       text,
+      textAlign: TextAlign.center,
       style: const TextStyle(
-        fontSize: 40,
-        color: Colors.white,
+        fontSize: 20,
+        color: Colors.blueGrey,
         fontStyle: FontStyle.italic,
-        fontWeight: FontWeight.w100,
+
       ),
     );
   }
---
+++ handshake/lib/connexion.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+/*
+class connexion extends StatefulWidget {
+  @override
+
+  PageConnexion createState() => PageConnexion();
+}
+
+class PageConnexion extends State<connexion> {
+  @override
+
+  void initState() {
+    super.initState();
+  }
+
+}
+*/
