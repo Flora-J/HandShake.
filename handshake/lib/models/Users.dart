132094d9405bd7105b6a7b935c3dbb4bc5cfbaa6
thibault <thibault.blanchard@edu.itescia.fr>
2022-03-24 10:24:44 +0100

    Update Users.dart
    add map for hobbies

--- handshake/lib/models/Users.dart
+++ handshake/lib/models/Users.dart
@@ -10,7 +10,8 @@ class Users{
   String email;
   String password;
   String organization;
-
-  Users ({required this.firstName, required this.lastName, required this.imageUrl, required this.organization, required this.address, required this.postCode, required this.city, required this.email, required this.password});
+  Map <int, String> hobbies;
+  
+  Users ({required this.firstName, required this.lastName, required this.imageUrl, required this.organization, required this.address, required this.postCode, required this.city, required this.email, required this.password, required  this.hobbies});
 
 }
