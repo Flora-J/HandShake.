b434926749548e64c96cb822747b7179388aac65
thibault <thibault.blanchard@edu.itescia.fr>
2022-03-25 12:00:57 +0100

    Update

--- handshake/ios/Runner/GeneratedPluginRegistrant.m
+++ handshake/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,10 +6,22 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<is_24_hour_format/Respect24HourPlugin.h>)
-#import <is_24_hour_format/Respect24HourPlugin.h>
+#if __has_include(<firebase_auth/FLTFirebaseAuthPlugin.h>)
+#import <firebase_auth/FLTFirebaseAuthPlugin.h>
 #else
-@import is_24_hour_format;
+@import firebase_auth;
+#endif
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_database/FLTFirebaseDatabasePlugin.h>)
+#import <firebase_database/FLTFirebaseDatabasePlugin.h>
+#else
+@import firebase_database;
 #endif
 
 #if __has_include(<sqflite/SqflitePlugin.h>)
@@ -33,7 +45,9 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [Respect24HourPlugin registerWithRegistrar:[registry registrarForPlugin:@"Respect24HourPlugin"]];
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseDatabasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseDatabasePlugin"]];
   [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
   [SurveyKitPlugin registerWithRegistrar:[registry registrarForPlugin:@"SurveyKitPlugin"]];
   [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
