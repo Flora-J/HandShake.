2833872e0974fa6b23e44675632b9bcd042a68f5
thibault <thibault.blanchard@edu.itescia.fr>
2022-03-25 12:26:58 +0100

    Amel add

--- handshake/lib/views/sign_up.dart
+++ handshake/lib/views/sign_up.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '../widgets/textfield_widget.dart';
+import '../../../widgets/textfield_widget.dart';
 import 'package:handshake/widgets/background_decoration.dart';
-import 'connections.dart';
+
 
 class SignupPage extends StatefulWidget {
 
