1a13112a346a0b8afbdd112b14cfab54226b5421
thibault <thibault.blanchard@edu.itescia.fr>
2022-03-28 10:37:21 +0200

    Hobbies_companions

---
+++ handshake/lib/views/hobbies_companions.dart
@@ -0,0 +1,157 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import '../widgets/textfield_widget.dart';
+import 'package:handshake/widgets/background_decoration.dart';
+
+
+class Hobbies extends StatefulWidget {
+
+  @override
+  _HobbiesState createState()=> _HobbiesState();
+
+}
+
+class _HobbiesState extends State<Hobbies> {
+
+  String dropdownValue = 'Musique';
+  final organisationController = TextEditingController();
+  final otherHobbieController = TextEditingController();
+
+
+  get decoration => null;
+  @override
+  Widget build(BuildContext context) {
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final handShakeRef = ref.child('/handShakeDb/loisirs');
+    List<String> activite = [];
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        elevation: 30,
+        title: const Text(
+          "Loisirs",
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Container(
+          decoration: fondDecoration(),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child:
+          Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Dis-nous en plus sur toi !',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),),
+              SizedBox(height: 20),
+              textFieldBasic(organisationController,"De quelle organisation viens-tu ?"),
+              marginColumn(),
+              SizedBox(height: 20,),
+              _buildRowHobbies(),
+              SizedBox(height: 20,),
+              textFieldBasic(otherHobbieController,"As-tu d'autre loisir ? Si oui, nommes-les"),
+              SizedBox(height: 20,),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Container(
+                  padding: EdgeInsets.only(top: 3,left: 3),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black)
+                      )
+                  ),
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    height:60,
+                    onPressed: () async{
+
+                      activite.add(dropdownValue);
+                      activite.add(otherHobbieController.text);
+
+                      try {
+                        await handShakeRef.set({
+                          'Organisation': organisationController.text,
+                          'Activité 1': activite[0],
+                          'Activité 2': activite[1]
+
+                        },
+
+                        );
+                        print("entry has been added");
+                      }catch(error){
+                        print('Entry has not been added : $error' );
+                      };
+
+
+
+                    },
+                    color: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)
+                    ),
+                    child: Text("Suivant",style: TextStyle(
+                      fontWeight: FontWeight.w600,fontSize: 16,
+                    ),),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Text(dropdownValue)
+            ],
+          ),
+        ), ),
+    );
+  }
+
+  SizedBox marginColumn() => const SizedBox(height: 20);
+
+  Widget _buildRowHobbies() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+
+      children: [
+        Text("As-tu des loisirs ?", style: TextStyle(fontSize: 18,color: Colors.white)),
+        SizedBox(
+          width: 50,
+        ),
+        DropdownButton<String>(
+          value: dropdownValue,
+          icon: const Icon(Icons.arrow_downward),
+          elevation: 16,
+          style: const TextStyle(color: Colors.black),
+          underline: Container(
+            height: 2,
+            color: Colors.black,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue!;
+            });
+          },
+          items: <String>['Musique', 'Lecture', 'Jeu vidéos', 'Activité en plein air','Séries / Films','Documentaire']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ],
+    );
+
+  }
+}
