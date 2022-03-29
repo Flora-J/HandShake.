2aeb61580a4a2f7fd8e15badd9dc392c3e34b10a
thibault <thibault.blanchard@edu.itescia.fr>
2022-03-29 09:36:16 +0200

    update

--- handshake/lib/models/announces.dart
+++ handshake/lib/models/announces.dart
@@ -1,9 +1,9 @@
-class announces{
+class Announces{
   String title;
   String date;
   String hour;
   String activity;
   String descrition;
 
-  announces({required this.title, required this.date, required this.hour, required this.activity, required this.descrition });
+  Announces({required this.title, required this.date, required this.hour, required this.activity, required this.descrition });
 }
