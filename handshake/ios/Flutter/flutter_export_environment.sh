14b4fa60d70ef72dde667e158b11ab2f8aaff2d5
AmalGuedda <guedda94320@gmail.com>
2022-03-29 13:10:24 +0200

    Update
    Update  of environment

--- handshake/ios/Flutter/flutter_export_environment.sh
+++ handshake/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/2.10.3/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/flora/Dropbox/Mac/Desktop/HandShake./handshake"
+export "FLUTTER_ROOT=/Applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Applications/MAMP/htdocs/HandShake./handshake"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -9,5 +9,5 @@ export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
-export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/flora/Dropbox/Mac/Desktop/HandShake./handshake/.dart_tool/package_config.json"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
