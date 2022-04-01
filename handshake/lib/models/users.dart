6315854186d8d8b8ef55e97856428f076a3e6c26
Flora JULIE <flora.juli88@gmail.com>
2022-04-01 13:45:44 +0200

    first push

--- handshake/lib/models/users.dart
+++ handshake/lib/models/users.dart
@@ -3,18 +3,29 @@ class Users {
   String lastName;
   String? imageUrl;
   String? address;
-  int postCode;
-  String city;
+  String? postCode;
+  String? city;
   String email;
-  String password;
   String organization;
-  Map <int, String> hobbies;
+  String Id;
+  //Map<int, String> hobbies;
 
   Users(
-      {required this.firstName, required this.lastName, required this.imageUrl, required this.organization, required this.address, required this.postCode, required this.city, required this.email, required this.password, required this.hobbies});
+      {required this.firstName,
+      required this.lastName,
+      required this.imageUrl,
+      required this.organization,
+      required this.address,
+      required this.postCode,
+      required this.city,
+      required this.email,
+      required this.Id
+      //required this.hobbies});
+      });
 
-  factory Users.fromRTBD(Map<String, dynamic> data){
-    return Users(firstName: data['FirstName'],
+  factory Users.fromRTBD(Map<String, dynamic> data) {
+    return Users(
+        firstName: data['FirstName'],
         lastName: data['LastName'],
         imageUrl: data['Photo'],
         organization: data['Organization'],
@@ -22,7 +33,8 @@ class Users {
         postCode: data['CP'],
         city: data['City'],
         email: data['Email'],
-        password: data['Password'],
-        hobbies: data['Hobbies']);
+        Id: data['Id']
+        //hobbies: data['Hobbies']);
+    );
   }
 }
